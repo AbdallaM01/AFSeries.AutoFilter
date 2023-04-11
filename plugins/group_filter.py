01c727ed1f612ed26f90fb49d9784faa8b2ff094
AbdallaM01 <100961487+AbdallaM01@users.noreply.github.com>
2023-04-11 21:03:21 +0200

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -269,7 +269,7 @@ async def auto_filter(client, msg, spoll=False):
     )
 
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
-    TEMPLATE = IMDB_TEMPLATE
+    TEMPLATE = settings['template']
     if imdb:
         cap = TEMPLATE.format(
             group = message.chat.title,
